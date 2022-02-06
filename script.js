7fd74525c84bb0e342eb574930bf685817186027
vasivuk <vasivuk@yahoo.com>
2022-02-06 19:20:55 +0100

    Drawing with mouse clicked functionality

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const container = document.querySelector('.grid-container');
+document.addEventListener('mousedown', () => mouseClicked = true);
+document.addEventListener('mouseup', () => mouseClicked = false);
 
 const button = document.querySelector("#btnGrid");
 button.addEventListener('click', initGrid);
+
+let mouseClicked = false;
 
 function initGrid(){
     container.innerHTML = '';
@@ -15,18 +19,33 @@ function initGrid(){
             const div = document.createElement('div');
             div.classList.add('square');
             rowDiv.appendChild(div);
+            div.addEventListener('mouseover', () => {
+                if(mouseClicked){
+                    div.classList.add('blackInk');
+                }
+            });
         }
     }
 }
 
-for(let row = 0; row < 4; row++){
+for(let row = 0; row < 16; row++){
     const rowDiv = document.createElement('div');
     rowDiv.classList.add("row");
     container.appendChild(rowDiv);
-    for(let col = 0; col < 4; col++){
+    for(let col = 0; col < 16; col++){
         const div = document.createElement('div');
         div.classList.add('square');
         rowDiv.appendChild(div);
+        div.addEventListener('mouseout', () => {
+            if(mouseClicked){
+                div.classList.add('blackInk');
+            }        
+        });
+        div.addEventListener('click', () => {
+            if(mouseClicked){
+                div.classList.add('blackInk');
+            }        
+        });
     }
 }
 
