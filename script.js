405ef7d0d7355ad34d71e24d902cb28dbe21d6c9
vasivuk <vasivuk@yahoo.com>
2022-02-06 12:53:42 +0100

    Box border added, margin/border of squares deleted

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('.grid-container');
 
-for(let row = 0; row < 4; row++){
+for(let row = 0; row < 8; row++){
     const rowDiv = document.createElement('div');
     rowDiv.classList.add("row");
     container.appendChild(rowDiv);
-    for(let col = 0; col < 4; col++){
+    for(let col = 0; col < 8; col++){
         const div = document.createElement('div');
         div.classList.add('square');
         rowDiv.appendChild(div);
