6e1742030407e5e8e856ace1290dcc5613a706fa
vasivuk <vasivuk@yahoo.com>
2022-02-06 13:09:53 +0100

    Added button for grid initialization

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const container = document.querySelector('.grid-container');
 
-for(let row = 0; row < 8; row++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add("row");
-    container.appendChild(rowDiv);
-    for(let col = 0; col < 8; col++){
-        const div = document.createElement('div');
-        div.classList.add('square');
-        rowDiv.appendChild(div);
+const button = document.querySelector("#btnGrid");
+button.addEventListener('click', initGrid);
+
+function initGrid(){
+    let size = prompt("Please enter the desired size of the grid");
+    for(let row = 0; row < size; row++){
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add("row");
+        container.appendChild(rowDiv);
+        for(let col = 0; col < size; col++){
+            const div = document.createElement('div');
+            div.classList.add('square');
+            rowDiv.appendChild(div);
+        }
     }
 }
+
