34094f1f3bbc73f4bea64002fc34ec8a6db9b6a7
vasivuk <vasivuk@yahoo.com>
2022-02-06 13:12:47 +0100

    Defalt size added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const button = document.querySelector("#btnGrid");
 button.addEventListener('click', initGrid);
 
 function initGrid(){
+    container.innerHTML = '';
     let size = prompt("Please enter the desired size of the grid");
     for(let row = 0; row < size; row++){
         const rowDiv = document.createElement('div');
@@ -14,6 +15,17 @@ function initGrid(){
             div.classList.add('square');
             rowDiv.appendChild(div);
         }
+    }
+}
+
+for(let row = 0; row < 4; row++){
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add("row");
+    container.appendChild(rowDiv);
+    for(let col = 0; col < 4; col++){
+        const div = document.createElement('div');
+        div.classList.add('square');
+        rowDiv.appendChild(div);
     }
 }
 
